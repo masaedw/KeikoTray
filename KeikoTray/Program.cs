64f1a8a767554a2c8af08a513b494f43e1ab460f
masaedw <masa.edw@gmail.com>
2012-09-08 23:38:41 +0900

    Simple system tray application

--- KeikoTray/Program.cs
+++ KeikoTray/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace KeikoTray
@@ -13,9 +12,28 @@ namespace KeikoTray
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var contextMenu1 = new ContextMenu();
+            var menuItem1 = new MenuItem();
+            var notifyIcon1 = new NotifyIcon();
+
+            contextMenu1.MenuItems.AddRange(new[] { menuItem1 });
+
+            menuItem1.Index = 0;
+            menuItem1.Text = "E&xit";
+            menuItem1.Click += (object s, EventArgs e) =>
+            {
+                notifyIcon1.Visible = false;
+                Application.Exit();
+            };
+
+            notifyIcon1.Icon = new Icon("tray.ico");
+
+            notifyIcon1.ContextMenu = contextMenu1;
+
+            notifyIcon1.Text = "Form1 (NotifyIcon example)";
+            notifyIcon1.Visible = true;
+
+            Application.Run();
         }
     }
 }
