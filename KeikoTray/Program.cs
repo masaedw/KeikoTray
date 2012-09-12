a3ed0985aa16d9674523679a6654184e3eda34f1
masaedw <masa.edw@gmail.com>
2012-09-12 14:36:26 +0900

    support multple keikos on one execution

--- KeikoTray/Program.cs
+++ KeikoTray/Program.cs
@@ -1,64 +1,41 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Reactive.Linq;
 using System.Windows.Forms;
 
 namespace KeikoTray
 {
-    static class Program
+    public class KeikoIcon
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
-        [STAThread]
-        static void Main(string[] args)
+        Keiko keiko_;
+        ContextMenu menu_;
+        IDisposable handle_;
+        NotifyIcon icon_;
+
+        public KeikoIcon(Keiko keiko, ContextMenu menu)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Usage: KeikoTray.exe keikoName");
-                Environment.Exit(1);
-            }
+            keiko_ = keiko;
+            menu_ = menu;
+            icon_ = new NotifyIcon();
+            icon_.ContextMenu = menu_;
+            icon_.Text = String.Format("[{0}]", keiko_.Name);
+            icon_.Visible = true;
+        }
 
-            var name = args[0];
+        public void Start()
+        {
+            var anim = new KeikoAnimation(icon => icon_.Icon = icon);
 
-            var contextMenu1 = new ContextMenu();
-            var menuItem1 = new MenuItem();
-            var notifyIcon1 = new NotifyIcon();
-
-            contextMenu1.MenuItems.AddRange(new[] { menuItem1 });
-
-            menuItem1.Index = 0;
-            menuItem1.Text = "E&xit";
-            menuItem1.Click += (object s, EventArgs e) =>
-            {
-                notifyIcon1.Visible = false;
-                Application.Exit();
-            };
-
-            notifyIcon1.ContextMenu = contextMenu1;
-
-            notifyIcon1.Text = String.Format("[{0}]", name);
-            notifyIcon1.Visible = true;
-
-            var keiko = new Keiko("http://virtualkeiko.herokuapp.com/", name);
-
-            if (!keiko.Exists())
-            {
-                Trace.WriteLine(String.Format("{0} doesn't exist.", name));
-                Environment.Exit(1);
-            }
-
-            var anim = new KeikoAnimation(icon => notifyIcon1.Icon = icon);
-
-            Observable.Interval(TimeSpan.FromSeconds(3.0))
-                .Select(_ => keiko.GetState())
+            handle_ = Observable.Interval(TimeSpan.FromSeconds(3.0))
+                .Select(_ => keiko_.GetState())
                 .DistinctUntilChanged()
                 .Subscribe(state =>
                 {
                     if (state.StartsWith("220"))
                     {
-                        notifyIcon1.ShowBalloonTip(1000, name, "Error", ToolTipIcon.Error);
+                        icon_.ShowBalloonTip(1000, keiko_.Name, "Error", ToolTipIcon.Error);
                     }
                     anim.SetState(state);
                 }, (Exception e) =>
@@ -70,9 +47,70 @@ namespace KeikoTray
                     }
                     else
                     {
-                        throw new Exception("unknown exception", e);
+                        End();
+                        throw new Exception(String.Format("unknown exception in {0}", keiko_.Name), e);
                     }
                 });
+        }
+
+        public void End()
+        {
+            icon_.Visible = false;
+            if (handle_ != null)
+            {
+                handle_.Dispose();
+            }
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: KeikoTray.exe keikoName [keikoName...]");
+                Environment.Exit(1);
+            }
+
+            var contextMenu1 = new ContextMenu();
+            var menuItem1 = new MenuItem();
+            contextMenu1.MenuItems.AddRange(new[] { menuItem1 });
+
+            menuItem1.Index = 0;
+            menuItem1.Text = "E&xit";
+
+            var url = "http://virtualkeiko.herokuapp.com/";
+
+            var keikos = args
+                .Select(name => new Keiko(url, name))
+                .Where(keiko => keiko.Exists())
+                .Select(keiko => new KeikoIcon(keiko, contextMenu1))
+                .ToArray();
+
+            if (!keikos.Any())
+            {
+                Trace.WriteLine("No keikos. bye.");
+                Environment.Exit(1);
+            }
+
+            menuItem1.Click += (object s, EventArgs e) =>
+            {
+                foreach (var tray in keikos)
+                {
+                    tray.End();
+                }
+                Application.Exit();
+            };
+
+            foreach (var tray in keikos)
+            {
+                tray.Start();
+            }
 
             Application.Run();
         }
